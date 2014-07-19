32c58fdd3de95148f0a9a61a2bb669b4afda394c
mikeplace <mikeplace@gmail.com>
2014-07-19 13:07:39 +0300

    lots of new stuff
    lots

--- js/directives/examples/button.js
+++
@@ -1,7 +0,0 @@
-angular.module('writer').directive('funButton',function(){
-    return{
-        restrict:'E',
-        template:"<button type='button'>Click me</button>",
-        replace: 'true'
-    };
-});
